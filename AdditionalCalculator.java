dd86b2f93b951ff0f30d07a41b05c499c94380f2
Fayaz github <shaikfayazbaba321@gmail.com>
2025-04-29 16:53:07 +0530

    Add files via upload

---
+++ AdditionalCalculator.java
@@ -0,0 +1,12 @@
+public class AdditionalCalculator {
+    public static void AddTwonumbers(int a, int b){
+        int c = a+b;
+        System.out.println(" sum : " +c);
+    
+    }
+    public static void main (String [] args ){
+
+    
+        AddTwonumbers(10,20);
+    }
+}
---
+++ VolumeCalculator.java
@@ -0,0 +1,10 @@
+public class VolumeCalculator {
+    public static void volumeOfCube(double side){
+        System.out.println("Volume:"+side);
+
+    }
+
+public static void main(String[] args) {
+    volumeOfCube(4*4*4);
+}
+}
